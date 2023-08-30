bd0cb2186e2a382ab5ecdf7f91770037b108ea2e
Vishal Sharma <vishalsharma34258@gmail.com>
2023-08-30 18:48:14 +0530

    Corrected the animations for the mobile applications

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
+let width = window.innerWidth;
+
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".about-col-1", {
-  x: -300,
-  opacity: 0,
-  scale: 1.4,
-  duration: 0.8,
-  scrollTrigger: {
-    trigger: ".about-col-1",
-    start: "top 50%",
-  },
-});
-
-gsap.from(".about-col-2", {
-  x: 200,
-  y: 100,
-  opacity: 0,
-  duration: 0.8,
-  scale: 1.4,
-  scrollTrigger: {
-    trigger: ".about-col-2",
-    start: "top 70%",
-  },
-});
+if (width < 600) {
+  gsap.from(".about-col-1", {
+    x: -300,
+    opacity: 0,
+    scale: 1.4,
+    duration: 0.8,
+    scrollTrigger: {
+      trigger: ".about-col-1",
+      start: 150,
+    },
+  });
+  gsap.from(".about-col-2", {
+    x: 200,
+    y: 100,
+    opacity: 0,
+    duration: 0.8,
+    scale: 1.4,
+    scrollTrigger: {
+      trigger: ".about-col-2",
+      start: 500,
+    },
+  });
+} else {
+  gsap.from(".about-col-1", {
+    x: -300,
+    opacity: 0,
+    scale: 1.4,
+    duration: 0.8,
+    scrollTrigger: {
+      trigger: ".about-col-1",
+      start: "top 50%",
+    },
+  });
+  gsap.from(".about-col-2", {
+    x: 200,
+    y: 100,
+    opacity: 0,
+    duration: 0.8,
+    scale: 1.4,
+    scrollTrigger: {
+      trigger: ".about-col-2",
+      start: "top 70%",
+    },
+  });
+}
 
 gsap.from(".services-list div", {
   opacity: 0,
